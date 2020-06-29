1b41855ecde50b4da25084a07942ddba90b6b1a8
Jokius <djok69@gmail.com>
2020-06-29 06:05:11 +0300

    Change style

--- components/games/GamesList.vue
+++ components/games/GamesList.vue
@@ -4,7 +4,7 @@
       <v-list-item :key="`game-${game.id}`" class="border-bottom">
         <v-list-item-content>
           <div class="title-grid">
-            <v-list-item-title v-html="game.name" />
+            <span class="game-title">{{ game.name }}</span>
             <v-btn color="success" max-width="150" @click="openGame(game.id)">Загрузить</v-btn>
             <template v-if="game.master.id === user.id">
               <v-btn color="grey lighten-1" max-width="50" link :to="configGame(game.id)">
@@ -17,7 +17,7 @@
             </template>
           </div>
           <v-list-item-subtitle>
-            <span>Мастер: </span>
+            <span class="list-title">Мастер:</span>
             <v-avatar size="24" color="indigo">
               <img
                 v-if="game.master.avatar.thumb"
@@ -34,16 +34,15 @@
             </v-avatar>
           </v-list-item-subtitle>
           <v-list-item-subtitle>
-            <span>Игроки: </span>
+            <span class="list-title">Игроки:</span>
             <span v-if="game.users.length === 0">Нет</span>
-            <template v-for="user in game.users">
-              <v-avatar :key="`user-${user.id}`" size="24" color="indigo">
+            <template v-else>
+              <v-avatar v-for="user in game.users" :key="`user-${user.id}`" size="24" color="indigo" class="user">
                 <img
-                  v-if="user.avatar.thumb"
-                  :src="user.avatar.thumb"
+                  :src="user.imgChat"
                   :alt="user.nickname"
+                  :title="user.nickname"
                 />
-                <v-icon v-if="!user.avatar.thumb" dark>mdi-account-circle</v-icon>
               </v-avatar>
             </template>
           </v-list-item-subtitle>
@@ -102,9 +101,24 @@
     display: grid;
     grid-template-columns: 1fr repeat(3, max-content);
     grid-column-gap: 2px;
+    margin-bottom: 5px;
+  }
+
+  .game-title {
+    font-size: 1.5rem;
+    line-height: 36px;
   }
 
   .border-bottom {
     border-bottom: 1px solid $black;
+  }
+
+  .user {
+    margin-right: 2px;
+  }
+
+  .list-title {
+    width: 65px;
+    display: inline-flex;
   }
 </style>
