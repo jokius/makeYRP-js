982bb1f1823425725f6191e635f03cb00d4998b6
Jokius <djok69@gmail.com>
2020-09-03 22:28:15 +0300

    Fix date

--- models/MessageModel.js
+++ models/MessageModel.js
@@ -3,14 +3,14 @@ import { UserModel } from './UserModel'
 export class MessageModel {
   id = null
   body = {}
-  created_at = ''
+  createdAt = ''
   user = {}
 
   setInfo({ data, included }) {
     this.id = data.id
     const attributes = data.attributes
     this.body = attributes.body
-    this.createdAt = attributes.created_at
+    this.createdAt = attributes.createdAt
     const userId = data.relationships.user.data.id
     const userData = included.find(item => item.type === 'shortUser' && item.id === userId)
     this.user = new UserModel().setInfo({ data: userData })
