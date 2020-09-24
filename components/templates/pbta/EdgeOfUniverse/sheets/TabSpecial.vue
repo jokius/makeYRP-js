ef90adcd85bb409a70e8dbb1bcaa06b875379cfd
Jokius <djok69@gmail.com>
2020-09-24 06:02:58 +0300

    Change tab special

--- components/templates/pbta/EdgeOfUniverse/sheets/TabSpecial.vue
+++ components/templates/pbta/EdgeOfUniverse/sheets/TabSpecial.vue
@@ -8,7 +8,7 @@
       <v-text-field
         v-if="item.type === 'textField'"
         :value="item.value"
-        :label="item.label"
+        :label="item.name"
         color="indigo"
         class="input name"
         flat
@@ -16,30 +16,23 @@
         @input="value => input(`specials[${specialIndex}].items[${index}].value`, value)"
         @change="saveSheet"
       />
-      <div v-if="item.type === 'block-line'" class="block-line">
+      <div v-if="item.type === 'blockLine'" class="block-line">
         <div
-          v-for="(block, blockIndex) in item.items"
+          v-for="(block, blockIndex) in item.list"
           :key="`special-${index}-block-${blockIndex}`"
           class="block"
         >
-          <img
-            v-if="block.itemType === 'icon'"
-            :src="`/images/pbta/EdgeOfUniverse/${block.path}`"
-            class="icon"
-            :title="block.label"
-            :alt="block.label"
-          />
-          <div v-if="block.itemType === 'text'" class="text-black">{{ block.text }}</div>
+          <div v-if="block.itemType === 'text'" class="text-black">{{ block.name }}</div>
           <v-text-field
             v-if="block.itemType === 'numberField'"
             :value="block.value"
-            :label="block.label"
+            :label="block.name"
             color="indigo"
             class="input number"
             flat
             hide-details
             type="number"
-            @input="value => input(`specials[${specialIndex}].items[${index}].items[${blockIndex}].value`, value)"
+            @input="value => input(`specials[${specialIndex}].items[${index}].list[${blockIndex}].value`, value)"
             @change="saveSheet"
           />
           <div v-if="block.itemType === 'description'" class="description" v-html="block.text" />
@@ -49,7 +42,7 @@
       <v-select
         v-if="item.type === 'select'"
         :items="item.list"
-        :label="item.label"
+        :label="item.name"
         class="special-select"
         color="black"
         :multiple="item.multiple"
@@ -63,7 +56,7 @@
         <summary class="black-title">{{ item.name }}</summary>
         <div class="blocks-grid">
           <div
-            v-for="(block, blockIndex) in item.items"
+            v-for="(block, blockIndex) in item.list"
             :key="`special-${index}-block-${blockIndex}`"
             class="block"
           >
@@ -72,7 +65,7 @@
                 v-for="(select, selectIndex) in block.selects"
                 :key="`${index}-${selectIndex}-selects`"
                 :items="select.list"
-                :label="select.label"
+                :label="select.name"
                 class="special-select"
                 color="black"
                 :multiple="select.multiple"
@@ -81,7 +74,7 @@
                 :value="select.value"
                 hide-details
                 @change="value =>
-                  selectSet(`specials[${specialIndex}].items[${index}].items[${blockIndex}].selects[${selectIndex}].value`,
+                  selectSet(`specials[${specialIndex}].items[${index}].list[${blockIndex}].selects[${selectIndex}].value`,
                             value)"
               />
             </div>
@@ -89,7 +82,7 @@
             <v-select
               v-if="block.itemType === 'select'"
               :items="block.list"
-              :label="block.label"
+              :label="block.name"
               class="special-select"
               color="black"
               :multiple="block.multiple"
@@ -97,7 +90,7 @@
               :chips="block.multiple"
               :value="block.value"
               hide-details
-              @change="value => selectSet(`specials[${specialIndex}].items[${index}].items[${blockIndex}].value`, value)"
+              @change="value => selectSet(`specials[${specialIndex}].items[${index}].list[${blockIndex}].value`, value)"
             />
 
             <move
@@ -105,137 +98,216 @@
               :sheet="sheet"
               :move="block"
               :index="blockIndex"
-              :path="`specials[${specialIndex}].items[${index}].items`"
+              :path="`specials[${specialIndex}].items[${index}].list`"
             />
           </div>
         </div>
       </details>
+      <details v-if="item.type === 'equipment'" class="pointer">
+        <summary class="black-title">{{ item.name }}</summary>
+        <div class="description" v-html="item.description" />
+        <div class="blocks-grid">
+          <equipment
+            v-for="(item, blockIndex) in item.list"
+            :key="`equipment-${specialIndex}-${index}-${blockIndex}`"
+            :sheet="sheet"
+            :item="item"
+            :index="blockIndex"
+            :path="`specials[${specialIndex}].items[${index}].list`"
+          />
+        </div>
+      </details>
+      <div v-if="item.type === 'counter'" class="counter">
+        <div class="special-title">{{ item.name }}</div>
+        <v-text-field
+          :value="item.current"
+          color="indigo"
+          class="input name"
+          flat
+          hide-details
+          type="number"
+          @input="value => input(`specials[${specialIndex}].items[${index}].current`, value)"
+          @change="saveSheet"
+        />
+        <v-text-field
+          :value="item.max"
+          color="indigo"
+          class="input name"
+          flat
+          hide-details
+          type="number"
+          @input="value => input(`specials[${specialIndex}].items[${index}].max`, value)"
+          @change="saveSheet"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-  import { mapState } from 'vuex'
+import { mapState } from 'vuex'
+import Move from '@/components/templates/pbta/EdgeOfUniverse/components/Move'
+import Equipment from '@/components/templates/pbta/EdgeOfUniverse/components/Equipment'
 
-  import Move from '../components/Move'
+export default {
+  name: 'TabSpecial',
 
-  export default {
-    name: 'TabSpecial',
-    components: { Move },
-    props: {
-      id: { type: String, required: true },
-      specialIndex: { type: Number, required: true },
-      special: { type: Object, required: true },
-    },
+  components: { Equipment, Move },
 
-    computed: {
-      ...mapState({
-        sheets: state => state.game.sheets,
-      }),
+  props: {
+    id: { type: String, required: true },
+    specialIndex: { type: Number, required: true },
+    special: { type: Object, required: true },
+  },
 
-      sheet: {
-        get() {
-          return this.sheets.find(sheet => sheet.id === this.id)
-        },
-      },
+  computed: {
+    ...mapState({
+      sheets: state => state.game.sheets,
+    }),
 
-      params: {
-        get() {
-          return this.sheet.params
-        },
+    sheet: {
+      get() {
+        return this.sheets.find(sheet => sheet.id === this.id)
       },
     },
 
-    methods: {
-      input(path, value) {
-        this.$store.commit('game/updateSheetParams',
-                           {
-                             id: this.sheet.id,
-                             path,
-                             value,
-                           })
+    params: {
+      get() {
+        return this.sheet.params
       },
+    },
+  },
 
-      selectSet(path, value) {
-        this.$store.commit('game/updateSheetParams', { id: this.sheet.id, path, value })
-        this.saveSheet()
-      },
-
-      saveSheet() {
-        this.$cable.perform({
-          channel: 'GameChannel',
-          action: 'change',
-          data: { ...this.sheet, type: 'sheet' },
+  methods: {
+    input(path, value) {
+      this.$store.commit('game/updateSheetParams',
+        {
+          id: this.sheet.id,
+          path,
+          value,
         })
-      },
     },
-  }
+
+    selectSet(path, value) {
+      this.$store.commit('game/updateSheetParams', { id: this.sheet.id, path, value })
+      this.saveSheet()
+    },
+
+    saveSheet() {
+      this.$cable.perform({
+        channel: 'GameChannel',
+        action: 'change',
+        data: { ...this.sheet, type: 'sheet' },
+      })
+    },
+  },
+}
 </script>
 
 <style scoped lang="scss">
-  @import '~assets/css/colors';
+@import '~assets/css/colors';
 
-  .pointer {
-    cursor: pointer;
-  }
+.pointer {
+  cursor: pointer;
+}
 
-  .specials-grid {
-    background-color: $grayC5;
-    overflow: auto;
-    display: grid;
-    grid-template-columns: 0.99fr;
-    grid-template-rows: max-content;
-    padding: 0 5px;
-    min-height: 570px;
-  }
+.specials-grid {
+  background-color: $grayC5;
+  overflow: auto;
+  padding: 0 5px;
+  min-height: 570px;
+}
 
-  .black-title {
+.black-title {
+  margin-top: 5px;
+  margin-bottom: 5px;
+  font-size: 20px;
+  font-weight: bold;
+}
+
+.black {
+  background-color: $black;
+  color: $white;
+  text-align: center;
+}
+
+.blocks-grid {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-column-gap: 15px;
+}
+
+.special-select {
+  padding: 0;
+  margin: 10px 0 10px;
+  background-color: $white;
+}
+
+.block {
+  margin-bottom: 15px;
+}
+
+.block-line {
+  display: inline-flex;
+}
+
+.icon {
+  width: 35px;
+  height: 35px;
+  margin-top: 10px;
+}
+
+.number {
+  width: 70px;
+  margin-left: 10px;
+}
+
+.text-black {
+  margin-top: 17px;
+  font-size: 20px;
+}
+
+.special-title {
+  font-weight: bold;
+  font-size: 15px;
+  margin: 0;
+}
+
+
+.black {
+  background-color: $black;
+  color: $white;
+  text-align: center;
+}
+
+.specials {
+  margin: 10px 5px 15px;
+}
+
+.special-title {
+  font-weight: bold;
+  font-size: 15px;
+  margin: 0;
+}
+
+.enable {
+  background-color: $black;
+}
+
+.counter {
+  margin: auto;
+  width: 50%;
+  display: grid;
+  grid-template-columns: max-content 50px 50px;
+  grid-column-gap: 5px;
+
+  .special-title {
     margin-top: 5px;
-    margin-bottom: 5px;
-    font-size: 20px;
-    font-weight: bold;
-    text-align: center;
   }
 
-  .black {
-    background-color: $black;
-    color: $white;
-    text-align: center;
-  }
-
-  .blocks-grid {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-column-gap: 15px;
-  }
-
-  .special-select {
+  .input {
     padding: 0;
-    margin: 10px 0 10px;
-    background-color: $white;
+    margin: 0;
   }
-
-  .block {
-    margin-bottom: 15px;
-  }
-
-  .block-line {
-    display: inline-flex;
-  }
-
-  .icon {
-    width: 35px;
-    height: 35px;
-    margin-top: 10px;
-  }
-
-  .number {
-    width: 70px;
-    margin-left: 10px;
-  }
-
-  .text-black {
-    margin-top: 17px;
-    font-size: 20px;
-  }
+}
 </style>
